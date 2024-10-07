59c87aa212245f5c8c61117301a8b042cf1794be
laibaiqbal702 <130080263+laibaiqbal702@users.noreply.github.com>
2024-10-07 20:50:16 +0500

    HomePage_attempts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import Navbar from './components/Navbar'; // Adjust the path based on your file structure
+import Filters from "./components/Filters";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Rentals from "./components/Rentals";
+
 function App() {
   return (
-    <div className="App">
-      {  /*Navbar */}
-        <Navbar />
-        {/*Filters */}
-        {/*Cards */}
-        
+    <div className="">
+      {/* Navbar */}
+      <Navbar />
+      {/* Filters */}
+      <div className="sm:mx-6 md:mx-10 lg:mx-12 px-3">
+        <Filters />
+        {/* Rentals */}
+        <Rentals />
+      </div>
+
+      {/* Footer */}
+      <Footer />
     </div>
   );
 }
