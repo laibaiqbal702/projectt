fcec3c3776ad4eaa50f3b1063990246a13ae0618
laibaiqbal702 <130080263+laibaiqbal702@users.noreply.github.com>
2024-10-07 19:50:17 +0500

    Logo thing

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from '../assets/logo.png'
+const Navbar = () => {
+    return <div>
+        
+        {/* Left */}
+        <div className="">
+        <img src={logo} className="w-40" />
+        </div>
+        {/* Middle  */}
+        {/* Right */}
+
+    </div>;
+}
+
+export default Navbar;
